820a878fa8061b6eaa97948a71c1636f02421a9f
izthy-512 <908945962@qq.com>
2023-06-29 01:43:04 +0800

    update MyTimer

--- modules/MyTimer.py
+++ modules/MyTimer.py
@@ -1,22 +1,25 @@
 class MyTimer:
-    def __init__(self, timer) -> None:
+    def __init__(self, timer):
         self.Timer = timer
         self.timers = []
         self.isRunning = []
 
     # Add a timer
-    def add(self, timerId: int, ticks: int, repeat=False):
+    def add(self, timerId, ticks, repeat=False):
+        while timerId >= len(self.timers):
+            self.timers.append([0, 0, 0])
+            self.isRunning.append(False)
         self.timers[timerId] = [timerId, ticks, repeat]
 
-    def start(self, timerId: int):
+    def start(self, timerId):
         if not self.isRunning[timerId]:
             self.isRunning[timerId] = True
             self.Timer.start(*self.timers[timerId])
 
-    def finish(self, timerId: int):
+    def finish(self, timerId):
         self.isRunning[timerId] = False
 
-    def stop(self, timerId: int):
+    def stop(self, timerId):
         self.Timer.stop(timerId)
         self.isRunning[timerId] = False
 
